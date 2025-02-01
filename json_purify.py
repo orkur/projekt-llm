a727d463897e6738d68ff669ac33a46a2f719ae9
orkur <orkur123@gmail.com>
2025-02-01 20:53:28 +0100

    update purify data

--- json_purify.py
+++ json_purify.py
@@ -3,15 +3,18 @@ from datetime import datetime
 
 def recode(s):
     return s.encode('latin1').decode('utf-8')
-def write(messages, out): 
+def write(messages, out, with_date=False): 
     for message in messages:
         if "content" in message.keys():
-            timestamp = message["timestamp_ms"]
-            date_time = datetime.fromtimestamp(timestamp//1000)
-            formatted_date = date_time.strftime('%Y-%m-%d %H:%M:%S')
             name = recode(message['sender_name'])
             content = recode(message['content'])
-            out.write(formatted_date + ' [' + name + ']: ' + content.replace('\n', ' ')+"\n" )
+            if with_date:
+                timestamp = message["timestamp_ms"]
+                date_time = datetime.fromtimestamp(timestamp//1000)
+                formatted_date = date_time.strftime('%Y-%m-%d %H:%M:%S')
+                out.write(formatted_date + ' [' + name + ']: ' + content.replace('\r\n', ' ').replace('\n', ' ')+"\n" )
+            else:
+                out.write('[' + name + ']: ' + content.replace('\r\n', ' ').replace('\n', ' ')+"\n" )
 
 messages = []
 for i in range(1,37):
@@ -23,5 +26,7 @@ for i in range(1,37):
 messages.sort( key= lambda x : x['timestamp_ms'])
 out_json = open("messages.json", "w")
 json.dump(messages, out_json, indent=4)
-out_clear = open("messages.txt", "w")
+out_clear = open("messages.txt", "w", encoding="utf-8")
 write(messages, out_clear)
+out_json.close()
+out_clear.close()
